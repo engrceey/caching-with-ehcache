a51a93800094075b586a32ee19f3afe7f48bbbdf
zurum <zurumogbonda@gmail.com>
2022-04-28 11:08:18 +0100

    initial commit

---
+++ src/main/java/com/cachingtut/learningcaching/LearningcachingApplication.java
@@ -0,0 +1,13 @@
+package com.cachingtut.learningcaching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningcachingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningcachingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cachingtut/learningcaching/LearningcachingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cachingtut.learningcaching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningcachingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
